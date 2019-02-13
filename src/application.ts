5ac09f162a028486f48bc352849aeda87b2b6fe3
Artur <exstrominer@gmail.com>
2019-02-13 21:55:17 +0300

    Use router as array & fix new request arrow function visibility area

--- src/application.ts
+++ src/application.ts
@@ -4,7 +4,7 @@ import { Router } from './router'
 
 export class Application {
   public server: http.Server
-  private _router: Router = new Router()
+  private _routers: Router[] = []
 
   constructor(opts: ApplicationOptions) {
     this.server = http.createServer()
@@ -12,11 +12,15 @@ export class Application {
   }
 
   private initListeners(): void {
-    this.server.addListener('request', this.onRequest)
+    this.server.addListener('request', (req, res) => this.onRequest(req, res))
   }
 
-  private onRequest(req: http.IncomingMessage, res: http.ServerResponse) {
-    this._router.handleRequest(req, res)
+  private onRequest(req: http.IncomingMessage, res: http.ServerResponse): void {
+    this._routers.forEach(router => router.handleRequest(req, res))
+  }
+
+  public use(router: Router): void {
+    this._routers.push(router)
   }
 
   public close(callback: (err: Error) => void): void {
