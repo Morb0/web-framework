be0c25c29a9b1cfe789725eb856385f7816e3294
Artur <exstrominer@gmail.com>
2019-02-14 00:11:34 +0300

    Update basic example

--- examples/basic/server.js
+++ examples/basic/server.js
@@ -1,12 +1,16 @@
 const WebFramework = require('../../dist/web-framework.umd')
 
 const app = new WebFramework.Application()
-const router = new WebFramework.Router()
+const router = new WebFramework.Router('/hello')
 
 app.use(router)
 
-router.get('/', (req, res) => {
-  res.end('Hello')
+router.get('/world', (req, res) => {
+  res.end('[GET] Hello')
+})
+
+router.post('/world', (req, res) => {
+  res.end('[POST] Hello')
 })
 
 app.listen(3000, () => console.log('Server start'))
